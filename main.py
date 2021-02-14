2ecca1d5b3e5c0f10094f8bc438ad8c29ec7ed4a
rpi4 <philliplangland@gmail.com>
2021-02-13 20:49:24 -0600

    added specific relay on/off functions

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from services.oauth_service import get_oauth_token
 from services.utils import c_to_f
 
 DHT22_1 = dht_sensor.DHT_SENSOR("DHT22", temp_format="C")
+
+ARTOO_HUB_URL: str = config.ARTOO_HUB_URL
 SYSTEM_STATE: str = config.SYSTEM_STATE
 temp: float = config.temp
 humidity: float = config.humidity
@@ -42,19 +44,23 @@ def read_sensor(sensor: dht_sensor.DHT_SENSOR):
     global sensor_data
     global temp
     global humidity
+    global ARTOO_HUB_URL
+
     try:
         temp = float(result[0])
         hum = float(result[1])
         sensor_data = {"temp": temp, "humidity": hum}
         try:
             global OAUTH_DATA
-            print("Making requests to Artoo Hub")
+            print("Making requests to Artoo Hub Server.")
 
             # if there is an access token and it is not expired, use it
             # else get a new token
             if OAUTH_DATA is None:
+                print("No access token, requesting token.")
                 set_oauth_data()
-            elif OAUTH_DATA["expiration"] < now:
+            elif OAUTH_DATA["expiration"] < time():
+                print("Access token expired, requesting new token.")
                 set_oauth_data()
 
                 # if OAUTH_DATA["expiration"] < now:
@@ -67,21 +73,21 @@ def read_sensor(sensor: dht_sensor.DHT_SENSOR):
             # This will eventually match to the main hub hostname
             # These requests need to match against the config for the particular hostname including the sensor_id, and types, along with auth code
             response = requests.post(
-                "http://localhost:8000/api/sensors/data/",
+                f"{ARTOO_HUB_URL}/api/sensors/data/",
                 json={"name": "temperature", "values": temp, "sensor_id": 1},
                 headers=HEADERS,
             )
             response2 = requests.post(
-                "http://localhost:8000/api/sensors/data/",
+                f"{ARTOO_HUB_URL}/api/sensors/data/",
                 json={"name": "humidity", "values": hum, "sensor_id": 1},
                 headers=HEADERS,
             )
         except HTTPError as error:
-            print(error)
+            print(f"{error}")
         finally:
             return sensor_data
     except:
-        print("Error when reading sensor")
+        print("Error when reading sensor.")
         sleep(2)
         read_sensor(sensor)
 
@@ -89,43 +95,48 @@ def temp_in_range(check_delay, active_delay):
     global SYSTEM_STATE
     global ACTIVE_SLEEP_LIMIT
     global ACTIVE_SLEEP_COUNTER
+    global TEMP_GOAL
+
+    relay_num = hvac.set_relay_nums(TEMP_GOAL)
 
     print("Temperature is in range.")
     if SYSTEM_STATE == "ACTIVE":
-        print("Turning off system")
+        print("Turning off system.")
         # turn system off since temp in range
         SYSTEM_STATE = "INACTIVE"
         # turn off appropriate relays
+        hvac.switch_relays(0, relay_num)
     else:
         SYSTEM_STATE = "INACTIVE"
     print("Delaying next check")
     # real delay will be ~ 2 minutes
     sleep(check_delay)
 
-
 def temp_outof_range(check_delay, active_delay):
     global SYSTEM_STATE
     global ACTIVE_SLEEP_LIMIT
     global ACTIVE_SLEEP_COUNTER
+    global TEMP_GOAL
+
+    relay_nums = hvac.set_relay_nums(TEMP_GOAL)
 
     print("Temperature is out of range.")
     # keep track of 'ACTIVE' state
     print("Checking settings...")
     if SYSTEM_STATE == "ACTIVE":
-        print("system already active")
-        print("System waiting: {}s", format(active_delay))
+        print(f"System already active. Waiting: {active_delay}s")
         sleep(active_delay)
         ACTIVE_SLEEP_COUNTER += 1
         if ACTIVE_SLEEP_COUNTER > ACTIVE_SLEEP_LIMIT:
-            print(
-                "Limit of {} tries reached. System shutoff.", format(ACTIVE_SLEEP_LIMIT)
-            )
+            print(f"Counter: {ACTIVE_SLEEP_COUNTER}. Limit of {ACTIVE_SLEEP_LIMIT} tries reached. System shutoff.")
             SYSTEM_STATE = "SHUTOFF"
+            hvac.switch_relays(0, relay_nums)
+
     else:
         print("Verifying and activating system")
         SYSTEM_STATE = "ACTIVE"
         # activate relays
-        hvac.test_relays()
+        hvac.switch_relays(1, relay_nums)
         # real delay will be > 2 minutes
         sleep(check_delay)
 
@@ -138,14 +149,14 @@ def main():
     global TEMP_GOAL
     global HEAT_SETTING
     global COOL_SETTING
-    check_delay = 300
-    active_delay = 500
+    check_delay = 400
+    active_delay = 600
     global ACTIVE_SLEEP_LIMIT
     global ACTIVE_SLEEP_COUNTER
 
     while True:
         try:
-            print("SYSTEM STATE: {}".format(SYSTEM_STATE))
+            print(f"SYSTEM STATE: {SYSTEM_STATE}")
             sensor_data = read_sensor(DHT22_1)
             if sensor_data is not None:
                 temp = int(c_to_f(sensor_data["temp"]))
@@ -166,8 +177,13 @@ def main():
                 else:
                     temp_in_range(check_delay, active_delay)
 
+        except KeyboardInterrupt:
+            print("Performing system cleanup.")
+            print("Shutting off all relays...")
+            hvac.switch_relays(0, [1, 2, 3, 4])
+            return
         except RuntimeError as error:
-            print("Runtime error, program stopped")
+            print("Runtime error, program stopped.")
             print(error.args[0])
         except:
             sleep(check_delay)
--- relays/hvac.py
+++ relays/hvac.py
@@ -6,6 +6,11 @@ DEVICE_BUS = 1
 DEVICE_ADDR = 0x10
 bus = smbus.SMBus(DEVICE_BUS)
 
+FAN = 1
+COLD = 2
+HEAT = 3
+EM_HEAT = 4
+
 def test_relays():
     for i in range(1,5):
         bus.write_byte_data(DEVICE_ADDR, i, 0xFF)
@@ -13,6 +18,45 @@ def test_relays():
         bus.write_byte_data(DEVICE_ADDR, i, 0x00)
         t.sleep(1)
     return
+
+def turn_on(relay: int):
+    bus.write_byte_data(DEVICE_ADDR, relay, 0xFF)
+    return
+
+def turn_off(relay: int):
+    bus.write_byte_data(DEVICE_ADDR, relay, 0x00)
+    return
+
+def switch_relay(status: int, relay: int):
+    if status == 1:
+        turn_on(relay)
+    elif status == 0:
+        turn_off(relay)
+    else:
+        return
+
+def switch_relays(status: int, relays: list):
+    for i in relays:
+        switch_relay(status, i)
+
+def set_relay_nums(temp_goal):
+    global FAN
+    global COLD
+    global HEAT 
+    global EM_HEAT 
+    relay_num: int
+    relay_nums: list = [FAN]
+
+    # Check if fan is on already before adding to list
+
+    if temp_goal == "COOL":
+        relay_num = COLD
+    elif temp_goal == "HEAT":
+        relay_num = HEAT
+
+    relay_nums.append(relay_num)
+    
+    return relay_nums
 
 # while True:
 #     try:
